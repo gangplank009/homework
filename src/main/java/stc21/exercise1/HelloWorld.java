814d8b33c15d2971646ed7e5c19ebf80fc5ce023
Pavel Ephimov <gangplank009@gmail.com>
2019-10-30 00:16:59 +0300

    1-1-Создал класс HelloWorld в пакете stc21.exercise1. Класс выводит в консоль сообщение "Hello World!".

---
+++ src/main/java/stc21/exercise1/HelloWorld.java
@@ -0,0 +1,13 @@
+package stc21.exercise1;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
